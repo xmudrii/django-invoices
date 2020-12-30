c7fb891ea052d1de98b5f317a41d9110bebad38b
Marko Mudrinić <mudrinic.mare@gmail.com>
2020-12-30 18:47:24 +0100

    Add registration form. Minor improvements
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- invoices/urls.py
+++ invoices/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', views.register, name='register'),
     path('', include('invoices_app.urls'))
 ]
---
+++ invoices/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(req):
+    if req.method == 'POST':
+        form = UserCreationForm(req.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data['username']
+            raw_password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=raw_password)
+            login(req, user)
+            return redirect('invoices_app:invoices')
+        else:
+            return render(req, 'registration/register.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        return render(req, 'registration/register.html', {'form': form})
